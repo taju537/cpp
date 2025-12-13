13d293bb6aa68ebd359ca5ac2d9c421779c5d3c0
Tajul Islam <mdtaijulislam1379@gmail.com>
2025-12-13 22:34:29 +0600

    message

---
+++ dsa/dds.cpp
@@ -0,0 +1,50 @@
+ #include <iostream>
+ #include <vector>
+ using namespace std;
+    void merge(vector<int>&arr , int st,int mid ,int end) {
+        vector<int>temp;
+        int i = st,j =mid+1;
+        while( i <=mid && j <=end) {
+            if (arr[i] <=arr[j]) {
+                temp.push_back(arr[i]);
+                i++;
+            }else {
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+        while (i <=mid) {
+            temp.push_back(arr[i]);
+                i++;
+        }
+        while (j <=end) {
+             temp.push_back(arr[j]);
+                j++;
+        }
+        for (int idx =0;idx <temp.size();idx++) {
+            arr[idx+st] =temp[idx];
+        }
+    }
+
+ void mergesort (vector<int>&arr , int st,int end) {
+    if (st <end) {
+        int mid =st +(end-st )/2;
+
+        mergesort(arr,st ,mid);
+        mergesort(arr,mid+1,end);
+
+        merge(arr,st,mid,end);
+;    }
+
+ }
+
+ int main (){
+    vector<int>arr ={12,31,35,8,32,17 };
+
+    mergesort(arr, 0 ,arr.size()-1);
+
+    for (int val : arr) {
+        cout <<val << " ";
+    }
+    cout << endl;
+ }
